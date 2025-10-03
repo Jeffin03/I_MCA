6ab29ca236cbc288b882e647a1418bd6c7ef0153
Jeffin03 <jeffyissac2203@gmail.com>
2025-10-03 08:42:44 +0530

    Update

---
+++ DSA/prog3.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<stdlib.h>
+int prec(char c);
+char stack[100];
+int top=-1;
+void push(char num);
+char pop();
+void infixtopostfix(char str[]);
+int main()
+{
+	char str[100];
+	int ch;
+	printf("\nEnter an expression:");
+	scanf("%s",str);
+	infixtopostfix(str);
+	return 0;
+}
+int prec(char c)
+{
+	if(c=='^')
+	{
+		return 3;
+	}
+	else if(c=='/'||c=='*')
+	{
+		return 2;
+	}
+	else if(c=='+'||c=='-')
+	{
+		return 1;
+	}
+	else
+	{
+		return -1;
+	}
+}
+void push(char num)
+{
+	stack[++top]=num;
+}
+char pop()
+{
+	char c=stack[top];
+	stack[top--]='\0';
+	return(c);
+}
+void infixtopostfix(char str[])
+{
+	int i=0;
+	while(str[i]!='\0')
+	{
+		if(str[i]>=65&&str[i]<=90||str[i]>=97&&str[i]<=122)
+		{
+			printf("%c", str[i]);
+		}
+		else if(str[i]=='(')
+		{
+			push(str[i]);
+		}
+		else if(str[i]==')')
+		{
+			char c;
+			do
+			{
+				c=pop();
+				if(c!='(')
+				{
+					printf("%c",c);
+				}
+			}while(c!='(');
+		}
+		else
+		{
+			while(top!=-1&&prec(str[i])<=prec(stack[top]))
+			{
+				printf("%c",pop());
+			}
+			push(str[i]);
+		}
+		i++;
+	}
+	while(top!=-1)
+	{
+		printf("%c",pop());
+	}
+	printf("\n");
+}
