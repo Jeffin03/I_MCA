8d00690cff8eb26b2a2dafd815414e40421b8b18
Jeffin03 <jeffyissac2203@gmail.com>
2025-11-15 17:54:13 +0530

    removed the comments for readability.

--- Java/program2/MultiLevelInheritanceDemo.java
+++ Java/program2/MultiLevelInheritanceDemo.java
@@ -1,111 +1,60 @@
-// Base class
-class Grandparent 
-{
-	// Private member: accessible only within the class
-	private String familySecret = "Hidden treaure locations";
+class Grandparent {
+    private String familySecret = "Hidden treasure locations";
+    protected String familyName = "Smith";
+    public int familyGeneration = 1;
 
-	// Protected member: accessinle in subclass and same package
-	protected String familyName = "Smith";
+    private void showSecret() {
+        System.out.println("Grandparent's Secret: " + familySecret);
+    }
 
-	// Public member
-	public int familyGeneration = 1;
+    protected void displayFamilyName() {
+        System.out.println("Family Name: " + familyName);
+    }
 
-	// Private Method
-	private void showSecret()
-	{
-		System.out.println("Grandparent's Secret: " + familySecret);
-	}
+    public void showGeneration() {
+        System.out.println("Family Generation: " + familyGeneration);
+    }
 
-	// Protected Method
-	protected void displayFamilyName()
-	{
-		System.out.println("Family Name: " + familyName);
-	}
-
-	//Public Method
-	public void showGeneration()
-	{
-		System.out.println("Family Generation: " + familyGeneration);
-	}
-
-
-	// Public method to access private data
-	public void revealSecret()
-	{
-		showSecret(); 
-	}
-
+    public void revealSecret() {
+        showSecret(); 
+    }
 }
 
-// Intermediate class (Parent)
-class Parent extends Grandparent
-{
-	// Protected Member
-	protected String parentRole = "Guiding the next generation";
+class Parent extends Grandparent {
+    protected String parentRole = "Guiding the next generation";
 
-	// Public method
-	public void showParentRole()
-	{
-		System.out.println("Parent Role: "+ parentRole);
-
-		// Accessing protected member of grandparent
-		System.out.println("Family Name from Grandparent: "+ familyName);
-
-		showGeneration();
-
-		// Cannot access private or method of Grandparent directly
-		// System.out.println(familySecret); // X Error
-		// showSecret(); // X error
-
-		// Didn't write the comment 		
-		revealSecret();
-
-	}
-
+    public void showParentRole() {
+        System.out.println("Parent Role: "+ parentRole);
+        System.out.println("Family Name from Grandparent: "+ familyName);
+        showGeneration();
+        revealSecret();
+    }
 }
 
-//Sub Class(Child)
-class Child extends Parent
-{
-	// public member
-	public String childName = "John";
+class Child extends Parent {
+    public String childName = "John";
 
-	public void showChildDetails()
-	{
-		System.out.println("Child Name: " + childName);
-
-		//accessing protected member of Parent
-		System.out.println("Parent Role: " + parentRole);
-
-		// accessing protected member of Grandparent
-		System.out.println("Family Name: " + familyName);
-
-		// accessing public method of Grandparent
-		showGeneration();
-
-		//accessing private member of Grandparent is not allowed directly
-		revealSecret(); 
-		
-	}
-
+    public void showChildDetails() {
+        System.out.println("Child Name: " + childName);
+        System.out.println("Parent Role: " + parentRole);
+        System.out.println("Family Name: " + familyName);
+        showGeneration();
+        revealSecret(); 
+    }
 }
 
-public class MultiLevelInheritanceDemo
-{
-	public static void main(String[] args)
-	{
-		Child child = new Child();
+public class MultiLevelInheritanceDemo {
+    public static void main(String[] args) {
+        Child child = new Child();
 
-		System.out.println("----Child Details----");
-		child.showChildDetails();
+        System.out.println("----Child Details----");
+        child.showChildDetails();
 
-		System.out.println("\n ---Parent Details ---");
-		Parent parent = new Parent();
-		parent.showParentRole();
+        System.out.println("\n ---Parent Details ---");
+        Parent parent = new Parent();
+        parent.showParentRole();
 
-		System.out.println("\naccessing public member directly");
-		System.out.println("Child Generation: " + child.familyGeneration);
-
-	}
-
+        System.out.println("\naccessing public member directly");
+        System.out.println("Child Generation: " + child.familyGeneration);
+    }
 }
